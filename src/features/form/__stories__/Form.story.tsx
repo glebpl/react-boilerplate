04d3ae7668ab62f3d29c54b47a8628269ff59342
gplakhotnik <gplakhotnik@almworks.com>
2022-12-22 18:37:20 +0300

    In progress

---
+++ src/features/form/__stories__/Form.story.tsx
@@ -0,0 +1,12 @@
+import { ComponentMeta } from '@storybook/react';
+import * as React from 'react';
+import { Form } from '../Form';
+
+export const Initial = {};
+
+const meta: ComponentMeta<typeof Form> = {
+  title: 'Features/Form',
+  component: Form
+};
+
+export default meta;
